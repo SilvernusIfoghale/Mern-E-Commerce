ab9d4d0992047d740e3053023482467c7e655829
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-09-13 12:43:42 +0100

    update footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-slate-200">
       <div className="container mx-auto p-4">
         <p className="text-center font-bold" title="Youtube Channel">
-          SilverDev &copy 2024
+          SilverDev &copy; 2024 Torbita MERN Project
         </p>
       </div>
     </footer>
